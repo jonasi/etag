435b79ff0e50bf7a24c97c39c1457cc428f2f2b6
Isao Jonas <isao.jonas@gmail.com>
2020-03-03 11:04:22 -0600

    support flusher

--- etag.go
+++ etag.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
+	"hash"
 	"io"
 	"net/http"
 )
@@ -12,37 +13,65 @@ import (
 // based off the contents of the body
 func Handler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		hsh := sha1.New()
-		buf := bytes.NewBuffer(nil)
 		bufw := &rw{
 			headers: w.Header(),
-			w:       io.MultiWriter(hsh, buf),
+			rw:      w,
+			hash:    sha1.New(),
+			buf:     bytes.NewBuffer(nil),
 		}
 
 		h.ServeHTTP(bufw, r)
 
-		etag := hex.EncodeToString(hsh.Sum(nil))
+		if bufw.flushed {
+			return
+		}
+
+		etag := hex.EncodeToString(bufw.hash.Sum(nil))
 		w.Header().Set("etag", etag)
 
 		if bufw.status != 0 {
 			w.WriteHeader(bufw.status)
 		}
 
-		if buf.Len() > 0 {
+		if bufw.buf.Len() > 0 {
 			// todo(isao) - what to do with error?
-			_, _ = w.Write(buf.Bytes())
+			_, _ = w.Write(bufw.buf.Bytes())
 		}
 	})
 }
 
 type rw struct {
 	headers http.Header
-	w       io.Writer
+	rw      http.ResponseWriter
+	hash    hash.Hash
+	buf     *bytes.Buffer
 	status  int
+	flushed bool
 }
 
 func (w *rw) Write(b []byte) (int, error) {
-	return w.w.Write(b)
+	if w.flushed {
+		return w.rw.Write(b)
+	}
+
+	if n, err := write(w.buf, b); err != nil {
+		return n, err
+	}
+
+	return write(w.hash, b)
+}
+
+func write(w io.Writer, b []byte) (int, error) {
+	n, err := w.Write(b)
+	if err != nil {
+		return n, err
+	}
+
+	if n != len(b) {
+		return n, io.ErrShortWrite
+	}
+
+	return n, nil
 }
 
 func (w *rw) WriteHeader(status int) {
@@ -51,4 +80,21 @@ func (w *rw) WriteHeader(status int) {
 
 func (w *rw) Header() http.Header {
 	return w.headers
+}
+
+func (w *rw) Flush() {
+	if f, ok := w.rw.(http.Flusher); ok {
+		// if this is the first flush, immediately write
+		// any buffered data to the underlying rw and flip
+		// the bit so all future writes go to the underlying rw
+		if !w.flushed {
+			if w.buf.Len() > 0 {
+				// todo(isao) - do something with this error?
+				w.rw.Write(w.buf.Bytes())
+			}
+			w.flushed = true
+		}
+
+		f.Flush()
+	}
 }
